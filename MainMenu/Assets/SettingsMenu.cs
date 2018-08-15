dca78410d071535e131388853cd31883d70bd7f5
alexwills1 <promoclimate@gmail.com>
2018-08-15 23:11:22 +1200

    Title Screen and Aspect Ratios

--- MainMenu/Assets/SettingsMenu.cs
+++ MainMenu/Assets/SettingsMenu.cs
@@ -2,13 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 		
 	public AudioMixer audioMixer;
 
+	public Dropdown resolutionDropdown;
+
+	Resolution[] resolutions;
+
+	void Start () 
+	{
+		resolutions = Screen.resolutions;
+
+		resolutionDropdown.ClearOptions ();
+
+		List<string> options = new List<string> ();
+
+		int currentResolutionsIndex = 0;
+		for (int i = 0; i < resolutions.Length; i++) 
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			options.Add (option);
+
+			if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) 
+			{
+				currentResolutionsIndex = i;
+			}
+		}
+
+		resolutionDropdown.AddOptions (options);
+		resolutionDropdown.value = currentResolutionsIndex;
+		resolutionDropdown.RefreshShownValue ();
+	}
+
+	public void SetResolution (int resolutionIndex)
+	{
+		Resolution resolution = resolutions[resolutionIndex];	
+		Screen.SetResolution (resolution.width, resolution.height, Screen.fullScreen);
+	}
+
 	public void SetVolume (float volume)
 	{
 		audioMixer.SetFloat ("MainVolume", volume);
+	}
+
+	public void SetFullscreen (bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
 	}
 }
