84c7630436a365be8203f2c69efeda8108194e2f
Colleen <colleenvillasis@gmail.com>
2018-08-23 12:40:22 +1200

    Manually Merging

---
+++ Ngeru Platformer/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+    }
+
+
+}
