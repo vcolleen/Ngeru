7f1c0ce3bdefdf2b15bc076707c382774adcfb2d
ZamoraXD <>
2018-09-04 21:09:17 +1200

    Tree level added
    added the basic elements to the tree boss fight

---
+++ Ngeru Platformer/Assets/Tree Level Schtuff/Branch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Branch : MonoBehaviour {
+
+    public GameObject ngeru;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ngeru.GetComponent<Health>().TakeDamage();
+        ngeru.GetComponent<Animation>().Play();
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        StartCoroutine(ColliderOff());
+    }
+
+    IEnumerator ColliderOff()
+    {
+        yield return new WaitForSeconds(1.5f);
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
---
+++ Ngeru Platformer/Assets/Tree Level Schtuff/TreeSchtuff.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeSchtuff : MonoBehaviour {
+
+    public GameObject pressE;
+    public GameObject ngeru;
+    public GameObject winner;
+    private int i = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pressE.activeSelf == true && Input.GetKeyDown(KeyCode.E))
+        {
+            ngeru.transform.position = new Vector3(-1.934f, -0.99f, 0);
+            i++;
+        }
+
+        if (i == 3)
+        {
+            winner.SetActive(true);
+            ngeru.transform.position = new Vector3(-1.934f, -0.99f, 0);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ngeru.GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            pressE.SetActive(true);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        pressE.SetActive(false);
+    }
+}
