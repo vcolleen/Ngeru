0aaa12c7604692042b12dd0664496fd2a45e82e6
alexwills1 <promoclimate@gmail.com>
2018-09-26 15:03:49 +1200

    Click Screen Sprite working
    It does need some adjusting to with how it fits the frame but thats it

---
+++ Ngeru Platformer/Assets/ClickSpriteScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSpriteScript : MonoBehaviour {
+
+    public Sprite One;
+    public Sprite Two;
+    public Sprite Three;
+    public Sprite Four;
+
+    float timer = 0.5f;
+    float delay = 0.5f;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = One;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == One)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Two;
+                timer = delay;
+                return;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == Two)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Three;
+                timer = delay;
+                return;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == Three)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Four;
+                timer = delay;
+                return;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == Four)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = One;
+                timer = delay;
+                return;
+            }
+        }
+	}
+}
