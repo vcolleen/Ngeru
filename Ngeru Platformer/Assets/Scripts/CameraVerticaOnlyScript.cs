8e5ab8719ca6bb3238f62c53a5f746b8da8f3312
Liv <livvysim@gmail.com>
2018-10-03 21:24:53 +1300

    wall platform layout

---
+++ Ngeru Platformer/Assets/Scripts/CameraVerticaOnlyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVerticaOnlyScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform targetToFollow;
+
+    void Update ()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Clamp(targetToFollow.position.y, 0f, 10f),
+            transform.position.z);
+    }
+}
