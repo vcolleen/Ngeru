0772f8eb9861214ba33f9b6269f164c87d386758
Colleen Villasis <colleenvillasis@gmail.com>
2018-10-08 22:33:17 +1300

    Camera

---
+++ Ngeru Platformer/Assets/PlayVideo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+
+    RawImage rawImageComp;
+    AudioSource audioS;
+
+	// Use this for initialization
+	void Start () {
+        rawImageComp = GetComponent<RawImage>();
+        audioS = GetComponent<AudioSource>();
+
+        PlayClip();
+	}
+	
+	void PlayClip() {
+        rawImageComp.texture = movie;
+        movie.Play();
+        audioS.clip = movie.audioClip;
+        audioS.Play();
+    }
+}
--- Ngeru Platformer/Assets/Scripts/CameraScript.cs
+++ Ngeru Platformer/Assets/Scripts/CameraScript.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
-    public GameObject player;       //Public variable to store a reference to the player game object
+    //what we're following
+    public Transform target;
 
+    Vector3 velocity = Vector3.zero;
 
-    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    public float smoothTime = .15f;
 
-    private void Start()
+    public bool YMaxEnabled = false;
+    public float YMaxValue = 0;
+
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
+
+    public bool YMinEnabled = false;
+    public float YMinValue = 0;
+
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+
+    private void FixedUpdate()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-    }
+        Vector3 targetPos = target.position;
+
+        if (YMinEnabled && YMaxEnabled)
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, YMaxValue);
+
+        else if (YMinEnabled)
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, target.position.y);
+
+        else if (YMaxEnabled)
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, YMaxValue);
+
+        if (XMinEnabled && XMaxEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+
+        else if (XMinEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, target.position.x);
+
+        else if (XMaxEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
 
 
-    void LateUpdate()
-    {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
     }
 
 
