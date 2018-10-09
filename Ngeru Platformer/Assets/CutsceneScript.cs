578eb62bc5718f55981fec2a8027d6b9d6518e4a
Connor <Connor.kortens@xtra.co.nz>
2018-10-10 12:07:46 +1300

    Added Cutscenes

---
+++ Ngeru Platformer/Assets/CutsceneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneScript : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	[SerializeField]
+	private string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Renderer>().material.mainTexture = movTexture;
+    movTexture.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (movTexture.isPlaying) {
+			return;
+		} else {
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
--- Ngeru Platformer/Assets/Scripts/TreeLevelTransition.cs
+++ Ngeru Platformer/Assets/Scripts/TreeLevelTransition.cs
@@ -21,6 +21,6 @@ public class TreeLevelTransition : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene("Tree Level");
+        SceneManager.LoadScene("RooftopCS");
     }
 }
