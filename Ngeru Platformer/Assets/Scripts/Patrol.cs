1e7657879bf0c292705c1faf04c507c21f079ba2
Colleen <colleenvillasis@gmail.com>
2018-10-11 14:16:33 +1300

    Park

--- Ngeru Platformer/Assets/Scripts/Patrol.cs
+++ Ngeru Platformer/Assets/Scripts/Patrol.cs
@@ -4,62 +4,66 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
+    //calling the animator
+    public Animator anim;
 
-	public float speed;
-	public float distance;
 
-    public float lifeSpan;
+
+    public float speed;
+    public float distance;
+
     private float timeAlive;
     private float spawnTime;
- 
-	private bool movingRight = true;
 
-	public Transform groundDetection;
+    private bool moving = true;
+
+    public Transform groundDetection;
 
     RandomEntranceScript rEntranceScript;
 
     void Awake()
     {
 
-        //rEntranceScript = GameObject.Find("AIController").GetComponent<RandomEntranceScript>();
-        //spawnTime = Time.time;
-        //timeAlive = 0f;
+        rEntranceScript = GameObject.Find("AIController").GetComponent<RandomEntranceScript>();
+        spawnTime = Time.time;
+        timeAlive = 0f;
 
     }
 
-	void Update()
+    void Update()
     {
 
         LifeCycle();
 
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-		RaycastHit2D groundInfo = Physics2D.Raycast (groundDetection.position, Vector3.down, distance);
-		if (groundInfo.collider == false)
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector3.down, distance);
+        if (groundInfo.collider == false)
+        //Debug.Log ("NotHittingAnything");
         {
-			if(movingRight == true)
+            if (moving == true)
             {
+                anim.SetBool("WalkRight", true);
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moving = false;
 
-				transform.eulerAngles = new Vector3(0, -180, 0);
-				movingRight = false;
-
-			}
+            }
             else
             {
-				transform.eulerAngles = new Vector3(0, 0, 0);
-				movingRight = true;
-			}
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moving = true;
+            }
 
-		}
+        }
 
-	}
+    }
 
-    void LifeCycle ()
+    void LifeCycle()
     {
 
         timeAlive = (Time.time - spawnTime);
 
-        if (timeAlive >= lifeSpan)
+        if (transform.position.x >= rEntranceScript.spawnPos.x && timeAlive >= 2)
         {
 
             Destroy(gameObject);
--- Ngeru Platformer/Assets/Scripts/RandomEntranceScript.cs
+++ Ngeru Platformer/Assets/Scripts/RandomEntranceScript.cs
@@ -1,70 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RandomEntranceScript : MonoBehaviour {
+public class RandomEntranceScript : MonoBehaviour
+{
 
-	public int rsTimer;
-	public bool isSpawned;
+    public int rsTimer;
+    public bool isSpawned;
     public float randomNumber;
     public float randomTime;
     public float minRange;
     public float maxRange;
+    public Vector3 spawnPos;
 
-	public GameObject humanAI;
+    public GameObject background;
+    public GameObject humanAI;
     public GameObject lineRenderer;
+    public GameObject hideUI;
 
-    
-	void Update ()
+    void start()
+    {
+        hideUI.GetComponent<Text>().enabled = false;
+    }
+
+    void Update()
     {
 
-		AITimer ();
-		SpawnTimer ();
+        AITimer();
+        SpawnTimer();
 
         if (isSpawned == false)
         {
 
             lineRenderer.GetComponent<LineRenderer>().enabled = false;
+            if ((randomTime - Time.time) <= 5f)
+            {
+                hideUI.GetComponent<Text>().enabled = true;
+            }
 
-        } else
+        }
+        else
         {
             lineRenderer.GetComponent<LineRenderer>().enabled = true;
+            hideUI.GetComponent<Text>().enabled = false;
+
         }
 
-	}
+    }
 
-	void AITimer ()
+    void AITimer()
     {
 
-		if (Time.time > rsTimer)
+        if (Time.time > rsTimer)
         {
 
-			rsTimer += 1;
-			Debug.Log(rsTimer);
+            rsTimer += 1;
 
-		}
-	}
+        }
+    }
 
-	void SpawnTimer ()
+    void SpawnTimer()
     {
+
         if (Time.time >= randomTime)
         {
 
             SpawnAI();
 
         }
-	}
+    }
 
-	void SpawnAI ()
+    void SpawnAI()
     {
 
         if (isSpawned == false)
         {
-            
-            Instantiate(humanAI, transform.position, Quaternion.identity);
+            float tileWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+            spawnPos = new Vector3(tileWidth / 2 + 1, transform.position.y, transform.position.z);
+            Instantiate(humanAI, spawnPos, Quaternion.identity);
             isSpawned = true;
             randomNumber = Random.Range(minRange, maxRange);
 
-        }        	
-	}
+        }
+    }
 }
