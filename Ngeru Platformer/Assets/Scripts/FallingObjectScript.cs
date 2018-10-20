727304afd73e8e54b56c76efe23a5dbfd5204366
Aran <ac.arancampbellhawker@gmail.com>
2018-10-20 19:47:59 +1300

    Added falling objects to scene
    Objects fall from both sides of the scene

--- Ngeru Platformer/Assets/Scripts/FallingObjectScript.cs
+++ Ngeru Platformer/Assets/Scripts/FallingObjectScript.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class FallingObjectScript : MonoBehaviour {
 
+	public float xForceR1 = 50f;
+	public float xForceR2 = 200f;
+	private float xForce;
+	public float yForce = 0f;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void Awake () {
+
+		xForce = Random.Range (xForceR1, xForceR2);
+
+		if (gameObject.transform.position.x <= -0.1) {
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (xForce, yForce));
+		} else {
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (xForce * -1, yForce));
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 
