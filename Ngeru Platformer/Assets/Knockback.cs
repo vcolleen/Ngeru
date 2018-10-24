5e312ea33784c1900892ab3a34111bcc8accd558
ZamoraXD <17122533@massey.ac.nz>
2018-10-24 16:30:49 +1300

    Knockback Improved

--- Ngeru Platformer/Assets/Knockback.cs
+++ Ngeru Platformer/Assets/Knockback.cs
@@ -7,35 +7,64 @@ public class Knockback : MonoBehaviour {
     public Rigidbody2D rgbd;
     public GameObject ngeru;
 
+    bool Frozen;
+    float FreezeTime;
+
     // Use this for initialization
     void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+    if (Frozen) {
+
+      ngeru.GetComponent<ControllerPlayerScript>().enabled = false;
+      //ngeru.GetComponent<Animator>().enabled = false;
+
+      rgbd.velocity = new Vector2(0,0);
+
+      if (Time.time > FreezeTime) {
+        Frozen = false;
+        ngeru.GetComponent<ControllerPlayerScript>().enabled = true;
+        //ngeru.GetComponent<Animator>().enabled = true;
+        ngeru.GetComponent<Animator>().SetBool("Knockback", false);
+        ngeru.GetComponent<Animator>().SetLayerWeight(3, 0);
+        ngeru.GetComponent<Animator>().SetLayerWeight(0, 1);
+        rgbd.GetComponent<Rigidbody2D>().gravityScale = 1;
+      }
+    }
+
 	}
 
     public void OnCollisionEnter2D(Collision2D col)
     {
-        ngeru.GetComponent<Animation>().Play("Flash");
-        KnockbackForce(0.03f, 60, ngeru.transform.position);
-        //StartCoroutine(Freeze());
+        //ngeru.GetComponent<Animation>().Play("Knockback");
+        ngeru.GetComponent<Animator>().SetBool("Knockback", true);
+        ngeru.GetComponent<Animator>().SetLayerWeight(3, 1);
+        ngeru.GetComponent<Animator>().SetLayerWeight(0, 0);
+        ngeru.GetComponent<Animator>().SetLayerWeight(1, 0);
+        ngeru.GetComponent<Animator>().SetLayerWeight(2, 0);
+        KnockbackForce(0.03f, 50, new Vector2(-5,0));
     }
 
     public void KnockbackForce(float duration, float power, Vector2 direction)
     {
+
         float timer = 0;
+        Frozen = true;
+        FreezeTime = Time.time + 1f;
 
         if (ngeru.GetComponent<ControllerPlayerScript>().isWalkingRight == true)
         {
 
-            while (duration > timer)
-            {
-                timer += Time.deltaTime;
-                rgbd.AddForce(new Vector2(direction.x * 150, direction.y + power));
-            }
+          while (duration > timer)
+          {
+              timer += Time.deltaTime;
+              rgbd.AddForce(new Vector2(direction.x * 170, direction.y + power));
+              rgbd.GetComponent<Rigidbody2D>().gravityScale = 10;
+          }
 
         } else if (ngeru.GetComponent<ControllerPlayerScript>().isWalkingLeft == true)
         {
@@ -43,7 +72,8 @@ public class Knockback : MonoBehaviour {
             while (duration > timer)
             {
                 timer += Time.deltaTime;
-                rgbd.AddForce(new Vector2(direction.x * -150, direction.y + power));
+                rgbd.AddForce(new Vector2(direction.x * -170, direction.y + power));
+                rgbd.GetComponent<Rigidbody2D>().gravityScale = 10;
             }
 
         } else
@@ -52,7 +82,8 @@ public class Knockback : MonoBehaviour {
             while (duration > timer)
             {
                 timer += Time.deltaTime;
-                rgbd.AddForce(new Vector2(direction.x * 150, direction.y + power));
+                rgbd.AddForce(new Vector2(direction.x * 170, direction.y + power));
+                rgbd.GetComponent<Rigidbody2D>().gravityScale = 10;
             }
 
         }
