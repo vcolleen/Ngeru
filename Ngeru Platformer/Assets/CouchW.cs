a0201e9179c45d688523babc2bd2a8267f018ef3
ZamoraXD <>
2018-10-28 11:16:45 +1300

    KitchenBugFixes

---
+++ Ngeru Platformer/Assets/CouchW.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CouchW : MonoBehaviour {
+
+    public GameObject wBut;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+
+        wBut.SetActive(true);
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        wBut.SetActive(false);
+    }
+}
