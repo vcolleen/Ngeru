b7ef33a01c6f4fdb7342a65e214cc9949c5e9c08
ZamoraXD <>
2018-10-27 21:17:24 +1300

    SphynxBugFixes

---
+++ Ngeru Platformer/Assets/ByeSphynx.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByeSphynx : MonoBehaviour {
+
+    public GameObject ngeru;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<SpriteRenderer>().color.a == 0)
+        {
+            ngeru.GetComponent<ControllerPlayerScript>().enabled = true;
+            gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Ngeru Platformer/Assets/SphynxCollider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphynxCollider : MonoBehaviour {
+
+    public GameObject ngeru;
+    public bool ngeruAnim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ngeruAnim)
+        {
+
+            ngeru.GetComponent<Animator>().SetBool("isIdle", true);
+
+            if (ngeru.GetComponent<Animator>().GetBool("isIdle") == true && ngeru.GetComponent<Animator>().GetBool("isWalkingLeft") == false && ngeru.GetComponent<Animator>().GetBool("isWalkingRight") == false)
+            {
+
+                if (Input.GetAxis("Trigger") == 1)
+                {
+                    ngeru.GetComponent<ControllerPlayerScript>().enabled = false;
+                }
+
+            }
+
+        } else
+        {
+            ngeru.GetComponent<ControllerPlayerScript>().enabled = true;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        ngeruAnim = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        ngeruAnim = false;
+    }
+}
