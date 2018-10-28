44e0816c05beb7d63b8a8f106b7ae2c2ae7e6e62
ZamoraXD <>
2018-10-29 00:00:35 +1300

    LucasCatFixes

--- Ngeru Platformer/Assets/DialogueJump.cs
+++ Ngeru Platformer/Assets/DialogueJump.cs
@@ -9,7 +9,7 @@ public class DialogueJump : MonoBehaviour {
     public GameObject dia3;
     public GameObject dia4;
     public GameObject dia5;
-    //public GameObject dia6;
+    public GameObject dia6;
     public GameObject dia7;
 
     // Use this for initialization
@@ -26,7 +26,7 @@ public class DialogueJump : MonoBehaviour {
             dia3.SetActive(false);
             dia4.SetActive(false);
             dia5.SetActive(false);
-            //dia6.SetActive(false);
+            dia6.SetActive(false);
             dia7.SetActive(false);
         } else
         {
@@ -35,7 +35,7 @@ public class DialogueJump : MonoBehaviour {
             dia3.SetActive(true);
             dia4.SetActive(true);
             dia5.SetActive(true);
-            //dia6.SetActive(true);
+            dia6.SetActive(true);
             dia7.SetActive(true);
 
         }
