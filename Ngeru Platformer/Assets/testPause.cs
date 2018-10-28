3a4fa1d5f70352f888f1eb7f940c2ad8bb0c35b7
ZamoraXD <>
2018-10-28 14:42:28 +1300

    WIP

---
+++ Ngeru Platformer/Assets/testPause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 0f)
+            {
+                Time.timeScale = 1f;
+            } else
+            {
+                Time.timeScale = 0f;
+            }
+        }
+	}
+}
